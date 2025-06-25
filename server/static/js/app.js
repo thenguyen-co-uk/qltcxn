4be2440ad3f746d708c578f2c88c1b3fbce88c6f
Tung Nguyen <nvntung@gmail.com>
2025-06-25 15:48:20 +0100

    Fix 422 Unprocessable Content - convert all dates to ISO8601

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation()
+
+function convertToDateISO8601(strDate) {
+    const parts = strDate.split("/");
+    const isoDate = new Date(parts[2], parts[1], parts[0]).toISOString();
+    return isoDate.split('T')[0];
+}
